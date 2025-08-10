b43930d596fbe52bb90ae0bde8fa619cdf7274fb
shennawy <ahmedshennawy997@gmail.com>
2025-08-10 21:10:07 +0300

    cache

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     include: ["react-helmet"],
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        // Cache busting - adds hash to filenames
+        entryFileNames: `[name]-[hash].js`,
+        chunkFileNames: `[name]-[hash].js`,
+        assetFileNames: `[name]-[hash].[ext]`,
+      },
+    },
   },
 });
