55185eeaf0e0023ab3732d76b58a265ddeb41662
shennawy <ahmedshennawy997@gmail.com>
2025-08-10 19:58:39 +0300

    usless

--- vite.config.js
+++ vite.config.js
@@ -4,31 +4,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
-      manifest: {
-        name: "Socially",
-        short_name: "Socially",
-        description: "Your social app",
-        theme_color: "#000",
-        icons: [
-          {
-            src: "pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "pwa-512x512.png",
-            type: "image/png",
-            sizes: "512x512",
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
   optimizeDeps: {
     include: ["react-helmet"],
   },
