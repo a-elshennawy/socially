4f81cc663f3590c8b6b533073aef6bba9bd69d5d
shennawy <ahmedshennawy997@gmail.com>
2025-08-10 19:55:19 +0300

    PWS

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,12 @@ import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js");
+  });
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
