bc2ed7b2a1459c09eedeb000a540c4c66f2e5575
shennawy <ahmedshennawy997@gmail.com>
2025-09-20 16:26:52 +0300

    padding inner navbar

--- src/components/Layout/NavBar.jsx
+++ src/components/Layout/NavBar.jsx
@@ -5,10 +5,10 @@ export default function NavBar() {
   return (
     <>
       <nav className="row justify-content-center align-items-center m-0">
-        <div className="col-lg-6 col-10 text-end">
+        <div className="col-lg-6 col-10 text-end p-0">
           <PostInput />
         </div>
-        <div className="col-lg-6 col-2 text-end">
+        <div className="col-lg-6 col-2 text-end p-0">
           <ThemeToggle />
         </div>
       </nav>
